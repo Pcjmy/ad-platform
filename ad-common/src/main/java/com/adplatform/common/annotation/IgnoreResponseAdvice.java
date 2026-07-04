221278c9d2743a5885eb276d477fab46db6eca5b
Pcjmy <1716045191@qq.com>
2026-07-04 19:12:04 +0800

    feat: 统一响应处理

---
+++ ad-common/src/main/java/com/adplatform/common/annotation/IgnoreResponseAdvice.java
@@ -0,0 +1,11 @@
+package com.adplatform.common.annotation;
+
+import java.lang.annotation.Target;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+@Target({ElementType.TYPE, ElementType.METHOD})
+@Retention(RetentionPolicy.RUNTIME)
+public @interface IgnoreResponseAdvice {
+}
