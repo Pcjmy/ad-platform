5ac87b5f151935500943cb0e80f50ea41504645a
Pcjmy <1716045191@qq.com>
2026-07-26 23:43:46 +0800

    feat: 广告创意实体类定义

---
+++ ad-sponsor/src/main/java/com/adplatform/adsponsor/entity/Creative.java
@@ -0,0 +1,58 @@
+package com.adplatform.adsponsor.entity;
+
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@TableName("ad_creative")
+public class Creative {
+
+    @TableId(type = IdType.AUTO)
+    private Long id;
+
+    @TableField("name")
+    private String name;
+
+    @TableField("type")
+    private Integer type;
+
+    @TableField("material_type")
+    private Integer materialType;
+
+    @TableField("height")
+    private Integer height;
+
+    @TableField("width")
+    private Integer width;
+
+    @TableField("size")
+    private Integer size;
+
+    @TableField("duration")
+    private Integer duration;
+
+    /** 审核状态 */
+    @TableField("audit_status")
+    private Integer auditStatus;
+
+    @TableField("user_id")
+    private Long userId;
+
+    @TableField("url")
+    private String url;
+
+    @TableField("createTime")
+    private Date createTime;
+
+    @TableField("updateTime")
+    private Date updateTime;
+}
