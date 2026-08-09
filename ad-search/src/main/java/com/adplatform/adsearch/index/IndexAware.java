a7d709648347a124fa391d107360969efaf40e0c
Pcjmy <1716045191@qq.com>
2026-08-09 23:59:02 +0800

    feat: 推广计划索引

---
+++ ad-search/src/main/java/com/adplatform/adsearch/index/IndexAware.java
@@ -0,0 +1,11 @@
+package com.adplatform.adsearch.index;
+
+public interface IndexAware<K, V> {
+    V get(K key);
+
+    void add(K key, V value);
+
+    void update(K key, V value);
+
+    void delete(K key, V value);
+}
