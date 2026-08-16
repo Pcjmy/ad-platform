53e2e7f6f51fc2af3d7a4dafb0eb5072ee422f1e
Pcjmy <1716045191@qq.com>
2026-08-16 22:35:04 +0800

    feat: 创意索引

---
+++ ad-search/src/main/java/com/adplatform/adsearch/index/creative/CreativeIndex.java
@@ -0,0 +1,50 @@
+package com.adplatform.adsearch.index.creative;
+
+import com.adplatform.adsearch.index.IndexAware;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+@Slf4j
+@Component
+public class CreativeIndex implements IndexAware<Long, CreativeObject> {
+
+    private static final Map<Long, CreativeObject> objectMap;
+
+    static {
+        objectMap = new ConcurrentHashMap<>();
+    }
+
+    @Override
+    public CreativeObject get(Long key) {
+        return objectMap.get(key);
+    }
+
+    @Override
+    public void add(Long key, CreativeObject value) {
+        log.info("CreativeIndex before add: {}", objectMap);
+        objectMap.put(key, value);
+        log.info("CreativeIndex after add: {}", objectMap);
+    }
+
+    @Override
+    public void update(Long key, CreativeObject value) {
+        log.info("CreativeIndex before update: {}", objectMap);
+        CreativeObject oldObject = objectMap.get(key);
+        if (oldObject == null) {
+            objectMap.put(key, value);
+        } else {
+            oldObject.update(value);
+        }
+        log.info("CreativeIndex after update: {}", objectMap);
+    }
+
+    @Override
+    public void delete(Long key, CreativeObject value) {
+        log.info("CreativeIndex before delete: {}", objectMap);
+        objectMap.remove(key, value);
+        log.info("CreativeIndex after delete: {}", objectMap);
+    }
+}
