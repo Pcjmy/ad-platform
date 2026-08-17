01b931159761dd4f12681c49d1affa027f3c8cee
Pcjmy <1716045191@qq.com>
2026-08-18 00:11:31 +0800

    feat: 导出表数据字段定义

---
+++ ad-common/src/main/java/com/adplatform/common/dump/DConstant.java
@@ -0,0 +1,23 @@
+package com.adplatform.common.dump;
+
+public class DConstant {
+
+    private DConstant() {
+    }
+
+    /**
+     * 数据导出目录
+     */
+    public static final String DATA_ROOT_DIR = "E:/mysql-data/";
+
+    /**
+     * 各表导出数据的文件名
+     */
+    private static final String AD_PLAN = "ad_plan.data";
+    private static final String AD_UNIT = "ad_unit.data";
+    private static final String AD_CREATIVE = "ad_creative.data";
+    private static final String AD_CREATIVE_UNIT = "ad_creative_unit.data";
+    private static final String AD_UNIT_IT = "ad_unit_it.data";
+    private static final String AD_UNIT_DISTRICT = "ad_unit_district.data";
+    private static final String AD_UNIT_KEYWORD = "ad_unit_keyword.data";
+}
