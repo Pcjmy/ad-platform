11bf11bf2f058e18d4100c7fb5d32bfbc281ca68
Pcjmy <1716045191@qq.com>
2026-08-29 18:51:07 +0800

    feat: 实现第二层级索引操作

--- ad-search/src/main/java/com/adplatform/adsearch/handler/AdLevelDataHandler.java
+++ ad-search/src/main/java/com/adplatform/adsearch/handler/AdLevelDataHandler.java
@@ -1,11 +1,53 @@
 package com.adplatform.adsearch.handler;
 
+import com.adplatform.adsearch.index.DataTable;
 import com.adplatform.adsearch.index.IndexAware;
+import com.adplatform.adsearch.index.adplan.AdPlanIndex;
+import com.adplatform.adsearch.index.adplan.AdPlanObject;
+import com.adplatform.adsearch.index.creative.CreativeIndex;
+import com.adplatform.adsearch.index.creative.CreativeObject;
 import com.adplatform.adsearch.mysql.constant.OpType;
+import com.adplatform.common.dump.table.AdCreativeTable;
+import com.adplatform.common.dump.table.AdPlanTable;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 public class AdLevelDataHandler {
+
+    public static void handleLevel2(AdPlanTable planTable, OpType type) {
+        AdPlanObject planObject = new AdPlanObject(
+                planTable.getId(),
+                planTable.getUserId(),
+                planTable.getPlanStatus(),
+                planTable.getStartDate(),
+                planTable.getEndDate()
+        );
+        handleBinlogEvent(
+                DataTable.of(AdPlanIndex.class),
+                planObject.getPlanId(),
+                planObject,
+                type
+        );
+    }
+
+    public static void handleLevel2(AdCreativeTable creativeTable, OpType type) {
+        CreativeObject creativeObject = new CreativeObject(
+                creativeTable.getAdId(),
+                creativeTable.getName(),
+                creativeTable.getType(),
+                creativeTable.getMaterialType(),
+                creativeTable.getHeight(),
+                creativeTable.getWidth(),
+                creativeTable.getAuditStatus(),
+                creativeTable.getAdUrl()
+        );
+        handleBinlogEvent(
+                DataTable.of(CreativeIndex.class),
+                creativeObject.getAdId(),
+                creativeObject,
+                type
+        );
+    }
 
     private AdLevelDataHandler() {
     }
