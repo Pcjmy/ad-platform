54a4f82abd347a44d5a5f0e1f3ab5d9e22ab64ff
Pcjmy <1716045191@qq.com>
2026-09-05 22:59:19 +0800

    feat: 启动对Binlog的监听

---
+++ ad-search/src/main/java/com/adplatform/adsearch/runner/BinlogRunner.java
@@ -0,0 +1,24 @@
+package com.adplatform.adsearch.runner;
+
+import com.adplatform.adsearch.mysql.BinlogClient;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+public class BinlogRunner implements CommandLineRunner {
+    private final BinlogClient client;
+
+    @Autowired
+    public BinlogRunner(BinlogClient client) {
+        this.client = client;
+    }
+
+    @Override
+    public void run(String... strings) throws Exception {
+        log.info("connecting to mysql start");
+        client.connect();
+    }
+}
