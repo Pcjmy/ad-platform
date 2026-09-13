f6146492d24881c9353b1d86236920a111f39799
Pcjmy <1716045191@qq.com>
2026-09-13 23:00:43 +0800

    feat: 增量数据投递前的准备工作

---
+++ ad-search/src/main/java/com/adplatform/adsearch/index/DataLevel.java
@@ -0,0 +1,19 @@
+package com.adplatform.adsearch.index;
+
+import lombok.Getter;
+
+@Getter
+public enum DataLevel {
+
+    LEVEL_2("2", "level 2"),
+    LEVEL_3("3", "level 3"),
+    LEVEL_4("4", "level 4");
+
+    private String level;
+    private String desc;
+
+    DataLevel(String level, String desc) {
+        this.level = level;
+        this.desc = desc;
+    }
+}
--- ad-search/src/main/java/com/adplatform/adsearch/utils/CommonUtils.java
+++ ad-search/src/main/java/com/adplatform/adsearch/utils/CommonUtils.java
@@ -1,8 +1,17 @@
 package com.adplatform.adsearch.utils;
 
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.time.DateUtils;
+
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
 import java.util.Map;
 import java.util.function.Supplier;
 
+@Slf4j
 public class CommonUtils {
 
     public static <K, V> V getOrCreate(K key, Map<K, V> map, Supplier<V> factory) {
@@ -17,5 +26,21 @@ public class CommonUtils {
         }
         result.deleteCharAt(result.length() -1);
         return result.toString();
+    }
+
+    public static Date parseStringDate(String dateString) {
+        try {
+            DateFormat dateFormat = new SimpleDateFormat(
+                    "EEE MMM dd HH:mm:ss zzz yyyy",
+                    Locale.US
+            );
+            return DateUtils.addHours(
+                    dateFormat.parse(dateString),
+                    -8
+            );
+        } catch (ParseException e) {
+            log.error("parseStringDate error: {}", dateString);
+            return null;
+        }
     }
 }
